3a025039bb9aaee6635e6f113327763cc038e721
r-o-h-i-t-11 <rohitbhandge7@gmail.com>
2024-02-03 12:08:33 +0530

    first commit

---
+++ quotegenerator/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='container'>
+      <div className="quote-box">
+        <h2>Quote of the day</h2>
+        <blockquote>Loading...</blockquote>
+        <span>Loading...</span>
+      </div>
+    </div>
+  )
+}
+
+export default App
